cbc81c848353782e206bbc704dc1fd2c81ba4ec0
Mark Pratley <markpratley@hotmail.com>
2015-03-09 23:41:19 +0000

    Init Commit

---
+++ ExploratoryData/Project2/plot2.R
@@ -0,0 +1,75 @@
+# Coursera exdata-012
+#   Mark Pratley
+#     Assignment #2
+#       Plot#2
+#        09-Mar-2015
+
+library(dplyr)
+
+# magic url & files names
+files   <- c("summarySCC_PM25.rds", "Source_Classification_Code.rds")
+url     <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+zipFile <- "data.zip"
+
+# Reads & Cleans Files - (Downloads if required)
+#  Returns a list containing the 2 files
+getFile <- function(file) {
+    
+    if (!file.exists(file)) {
+        print("File not found - downloading zips...")
+        getFiles()
+    }
+    
+    if (!file.exists(file))
+        stop("Can't find or get files")
+    
+    print(paste(file, "found - loading data into R..."))    
+    ret = readRDS(file)
+}
+
+# Downloads & Unzips data to working directory  - called if not already there
+getFiles <- function() {
+    
+    download.file(url, zipFile)
+    unzip(zipFile, overwrite = TRUE)
+}
+
+## File reading and plotting begins here ##
+
+# Get Files  will be downloaded if required
+if (!exists("NEI"))
+    NEI <- getFile("summarySCC_PM25.rds")
+if (!exists("SCC"))
+    SCC <- getFile("Source_Classification_Code.rds")
+
+# TESTING TEMP
+bTESTING = TRUE
+nTesting = 100000
+if (bTESTING & nrow(NEI)<nTesting) {
+    x <- sample(1:nrow(NEI), nTesting, replace=F)
+    NEI <- NEI[x,]
+}
+# End Temp
+
+# Create year category
+NEI$year.cat <- factor(NEI$year)
+
+# Check Folder
+# Check if dir exists, if not create
+if (! file.exists("./myplots")) {
+    dir.create("./myplots")
+}
+#Create PNG device
+png("myplots/plot2.png", width = 480, height = 480,  bg = "transparent")
+
+#Draw the histogram to the PNG device
+Baltimore <- filter(NEI, fips == "24510")
+years <- tapply(Baltimore$E, Baltimore$year.cat, sum)
+barplot(years,
+        main = expression('Total Emissions in Baltimore of PM'[2.5]),
+        xlab = "Year",
+        ylab = "Total Emissions",
+        col = "brown")
+
+#Close PNG device
+dev.off()
