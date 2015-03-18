5ab2e2aae988d1a72d87f8d8568662097b6bd1b0
Mark Pratley <markpratley@hotmail.com>
2015-03-18 22:05:31 +0000

    Updated CaseStudy.R

--- ExploratoryData/CaseStudy/CaseStudy.R
+++ ExploratoryData/CaseStudy/CaseStudy.R
@@ -38,10 +38,11 @@ names(pm1) <- make.names(names1[[1]])
 dates <- pm1$Date
 dates <- strptime(dates, format = "%Y%m%d")
 
-
+# Plot a comparison of New York 1999 & 2012
 site0 <- unique(subset(pm0, State.Code==36, c(County.Code, Site.ID)))
 site1 <- unique(subset(pm1, State.Code==36, c(County.Code, Site.ID)))
 
+# Find a good site which works in both 1999 & 2012
 site0 <- paste(site0[,1], site0[,2], sep=".")
 site1 <- paste(site1[,1], site1[,2], sep=".")
 both <- intersect(site0, site1)
@@ -66,6 +67,38 @@ x1sub <- pm1sub$Sample.Value
 
 plot(dates0, x0sub)
 plot(dates1, x1sub)
+
+# Plot side by side graphs
+par(mfrow = c(1,2), mar = c(4, 4, 2, 1))
+range <- range(x0sub, x1sub, na.rm=T)
+
+plot(dates0,x0sub, pch=20, ylim=range)
+abline(h = median(x0sub, na.rm=T))
+
+plot(dates1,x1sub, pch=20, ylim=range)
+abline(h = median(x1sub, na.rm=T))
+
+# State averages for all states
+stav0 <- tapply(pm0$Sample.Value, pm0$State.Code, mean, na.rm=T)
+stav1 <- tapply(pm1$Sample.Value, pm1$State.Code, mean, na.rm=T)
+
+d0 <- data.frame(state=names(stav0), mean=stav0)
+d1 <- data.frame(state=names(stav1), mean=stav1)
+m <- merge(d0, d1, by = "state")
+names(m) <- c("state", "1999", "2012")
+
+# reset par
+par(mfrow=c(1,1))
+
+# create graph and first set of points
+plot( rep(1999, 53), m$"1999", xlim = c(1998, 2013))
+
+# 2nd set of points
+points( rep(2012, 53), m$"2012", xlim = c(1998, 2013))
+
+# draw lines between them
+segments( rep(1999, 53), m$"1999", rep(2012, 53), m$"2012")
+
 
 
 
